ed91126e3b69f3d5cb5617f99d774c7e7bce79ba
Jakub <jradzik4@gmail.com>
2021-12-11 13:58:30 +0100

    repair gitignore

--- dist/notepad/notepad.resolver.d.ts
+++
@@ -1,8 +0,0 @@
-import { Notepad } from 'src/graphql';
-import { NotepadService } from './notepad.service';
-export declare class NotepadResolver {
-    private notepadService;
-    constructor(notepadService: NotepadService);
-    notepad(id: string): Notepad;
-    notepads(): Notepad[];
-}
--- dist/notepad/notepad.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Notepad } from 'src/graphql';
-export declare class NotepadService {
-    private readonly notepads;
-    findAll(): Notepad[];
-    findOne(id: string): Notepad;
-}
